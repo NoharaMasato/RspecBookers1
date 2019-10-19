bf9018990e4f3da437cb8f39e3f78579abb0834a
NoharaMasato <masato338914914@gmail.com>
2019-10-19 15:55:23 +0900

    rubocopを直した

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :book do
-    sequence(:title) { |n| "title#{n}"}
-    sequence(:body) { |n| "body#{n}"}
+    sequence(:title) { |n| "title#{n}" }
+    sequence(:body) { |n| "body#{n}" }
   end
 end
